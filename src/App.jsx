999836c99f3312276afb407bc3f9115eba0f1d9b
howItPhills <vlad-man001@mail.ru>
2022-02-25 15:12:57 +0300

    made Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+
+import './App.css'
 
 const App = () => {
    return (
-      <div>hi</div>
+      <div className='app'>
+         <Navbar />
+         <div className='main'>
+
+         </div>
+         <div className='footer'>
+
+         </div>
+      </div>
    )
 }
 
