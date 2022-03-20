ba0cda7082981602725904de08febf0e75abc35f
howItPhills <vlad-man001@mail.ru>
2022-03-20 13:55:36 +0300

    modified Cryptocurrency component and added count variable to cryptoApi

--- src/components/Cryptocurrencies/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies/Cryptocurrencies.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Card, Col, Input, Row } from 'antd'
+import millify from 'millify'
+import { Link } from 'react-router-dom'
+import { useGetCryptosQuery } from '../../services/cryptoApi'
 
-const Cryptocurrencies = () => {
+const Cryptocurrencies = ({ simplified }) => {
+
+  const count = simplified ? 10 : 100
+
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count)
+
+  const [cryptos, setCryptos] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    const filteredCryptos = cryptosList?.data?.coins.filter(coin => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    setCryptos(filteredCryptos)
+  }, [cryptosList, searchTerm])
+
+
   return (
-    <div>Cryptocurrencies</div>
+    <div>
+      {!simplified ?
+        <div className='search-crypto'>
+          <Input placeholder='Search Cryptocurrency' onChange={(e) => setSearchTerm(e.target.value)} />
+        </div> :
+        null
+      }
+      <Row gutter={[32, 32]} className='crypto-card-container'>
+        {cryptos?.map(currency => (
+          <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.uuid}>
+            <Link to={`/cryptocurrencies/${currency.uuid}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img src={currency.iconUrl} className='crypto-image' alt='crypto' />}
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {millify(currency.change)}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </div >
   )
 }
 
--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -13,7 +13,7 @@ const { Title } = Typography
 
 const Homepage = () => {
 
-   const { data, isFetching } = useGetCryptosQuery()
+   const { data, isFetching } = useGetCryptosQuery(10)
    const stats = data?.data?.stats
 
    if (isFetching) return <div>Loading...</div>
@@ -34,12 +34,12 @@ const Homepage = () => {
             <Title level={2} className='home-title'>Top 10 Cryptocurrencies in the world</Title>
             <Title level={3} className='show-more'><Link to='/cryptocurrencies'>Show More</Link></Title>
          </div>
-         <Cryptocurrencies simplifed />
+         <Cryptocurrencies simplified />
          <div className='home-heading-container'>
             <Title level={2} className='home-title'>Latest Crypto News</Title>
             <Title level={3} className='show-more'><Link to='/news'>Show More</Link></Title>
          </div>
-         <News simplifed />
+         <News simplified />
       </div>
    )
 }
